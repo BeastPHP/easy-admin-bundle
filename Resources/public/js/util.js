30b48469b90d658f3a762d32f74ce3ccb7b71231
xin.guo <maxwell.guo@xgimi.com>
2018-10-29 10:32:31 +0800

    refactor(增加返回状态): 增加返回状态

--- Resources/public/js/util.js
+++ Resources/public/js/util.js
@@ -1,45 +1,47 @@
-var show500Warning = function(){
-    toastr.error("您的修改未保存成功","消息提示");
-}
+const show500Warning = function () {
+    toastr.error("您的修改未保存成功", "消息提示");
+};
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".request-ajax-for-active").off("click").on("click",function(){
-        var url  = $(this).data('url');
+    $('.request-ajax-for-active').off('click').on('click', function () {
+        const url = $(this).data('url');
         $.ajax({
-            type: "POST",
+            type: 'POST',
             url: url,
-            dataType: "json",
-            success: function(response){
-                if(response.status == '1'){
-                    toastr.success("修改成功","消息提示");
-                }else{
+            dataType: 'json',
+            success: function (response) {
+                if (response.status === 'T') {
+                    toastr.success('修改成功', '消息提示');
+                } else if (response.status === '1') {
+                    toastr.success('修改成功', '消息提示');
+                } else {
                     show500Warning();
                 }
             },
-            failure: function(errMsg) {
+            failure: function (errMsg) {
                 show500Warning();
             }
         });
     });
 
-    $(".delete_single_button").off("click").on("click",function(){
-        var dialogTitle   = $(this).data('title') || '删除提醒';
-        var dialogMessage = $(this).data('message') || '您确定要删除吗？所有与之关联的数据都会被删除';
-        var deleteUrl = $(this).attr('href');
+    $(".delete_single_button").off("click").on("click", function () {
+        const dialogTitle = $(this).data('title') || '删除提醒';
+        const dialogMessage = $(this).data('message') || '您确定要删除吗？所有与之关联的数据都会被删除';
+        const deleteUrl = $(this).attr('href');
         $.confirm({
             title: dialogTitle,
             content: dialogMessage,
             buttons: {
-                cancel:{
+                cancel: {
                     text: '取消',
-                    action: function(){
+                    action: function () {
                     }
                 },
-                confirm:{
+                confirm: {
                     btnClass: 'btn-warning',
                     text: '确认',
-                    action: function(){
+                    action: function () {
                         return location.href = deleteUrl;
                     }
                 },
@@ -49,12 +51,12 @@ $(document).ready(function(){
     });
 
 
-    $('.submit').click(function(){
-        $("form").submit();
+    $('.submit').click(function () {
+        $('form').submit();
     });
 
-    $('.huan_yue_tip').click(function(){
+    $('.huan_yue_tip').click(function () {
         $message = $(this).data('tip');
         layer.tips($message, $(this));
     });
-})
+});
